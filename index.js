466e80f5a9ee533f3ccb2456d112c650052419c0
Diego Venas <diegovenas61@gmail.coerwf>
2023-05-05 17:40:28 -0300

    Estilos

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var btn = document.querySelector('#btn');
 
-btn.addEventListener("click", function() {
-    var div = document.querySelector('.sessao');
-    
-  if(div.style.display === "none") {
-        div.style.display = "flex";
-        console.log('ooi ')
-    } else {
-      div.style.display = "none";
-  }
-  ;
-  // if (div.style.height === '200px'){
-  //   div.style.height = '300px'
-  // }
-  // else{div.style.height = '200px'}
-  // div = document.querySelector('.card').style.height = '300px'
-});
 
+function openModal(){
+  document.querySelector(".modal").style.display = "flex";
+  // console.log('ooi')
+
+}
+
+
+function closeModal(){
+  document.querySelector(".modal").style.display = "none";
+  // console.log('ooi')
+}
