1d2ba5e91542a2fdca1f078d5b6be6e8281f518a
Diego Venas <diegovenas61@gmail.coerwf>
2023-05-12 17:26:05 -0300

    node

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
+import express from "express";
+import * as path from "path";
+import { fileURLToPath } from "url";    
 
 
-function openModal(){
-  document.querySelector(".modal").style.display = "flex";
-  // console.log('ooi')
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = 3000;
 
-}
+app.listen(PORT, () => {
+  console.log('\x1b[36m%s\x1b[0m', 'Servidor rodando na porta ' + PORT + " \u2714 \u2714");
+});
+
+app.use("/rota1", express.static(path.join(__dirname, "client")));
 
 
-function closeModal(){
-  document.querySelector(".modal").style.display = "none";
-  // console.log('ooi')
-}
+
+app.get("/get", (req, res) => {
+  res.send({nome: "diefgo",
+sobrenome:  "venas"});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function openModal() {
+//   document.querySelector(".modal").style.display = "flex";
+//   // console.log('ooi')
+// }
+// function closeModal() {
+//   document.querySelector(".modal").style.display = "none";
+//   // console.log('ooi')
+// }
